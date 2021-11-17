f6ce5cb09609dc9a221267a4ab26ae01ba86bc39
AldiZulfikar <aldizulfikar04@gmail.com>
2021-11-17 15:14:24 +0700

    deploy

---
+++ database/seeders/userSeede.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class userSeede extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Aldi',
+            'email' => 'aldizulfikar04@gmail.com',
+            'password' => Hash::make('cuanterus'),
+            'roles' => 'ADMIN',
+            'username' => 'AldiZulfikar',
+        ]);
+    }
+}
