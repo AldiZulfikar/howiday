ed59a6f7e7d0e26a0fa42132db2a7a0fa5c90527
AldiZulfikar <aldizulfikar04@gmail.com>
2021-11-17 17:33:14 +0700

    make it better

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     @include('includes.frontend.style')
 </head>
 
-<body class="dark:bg-gray-800 body mx-10">
+<body class="dark:bg-gray-800 body">
 
     @include('includes.frontend.darkmode-toggle')
 
